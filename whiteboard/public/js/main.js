286c83b692ffb8c4eb2a9f09544046f1db06bae1
rmg0070 <rmg0070@gmail.com>
2023-04-15 17:11:21 -0400

    updating index code and  main.js

--- whiteboard/public/js/main.js
+++ whiteboard/public/js/main.js
@@ -56,4 +56,86 @@
         });
 
     });
+
+//var socket = io.connect('http://localhost:3000');
+    var socket = io();
+
+    window.wbSendMessage = function (msg) {
+        whiteboard.history().add(msg);
+        socket.emit('send_wb', msg);
+    };
+
+    window.changeWbHistory = function (operation) {
+        socket.emit('change_wb_history', operation);
+    };
+
+    socket.on('show_rooms', function (rooms) {
+        var selectRooms = $("#select-rooms");
+        selectRooms.html("");
+        for (var key in rooms) {
+            selectRooms.append('<option>' + key + '</option>');
+        }
+    });
+
+    socket.on('notification_message', function (label, content) {
+        $('#notification-div').fadeIn(1000).fadeOut(5800);
+        $('#notification-content').html('<strong>' + label + '</strong>' + "  " + content);
+    });
+
+
+    socket.on('update_users', function (users) {
+        var tableUsers = $("#table-users");
+        $("#users-badge").html(users.length);
+        tableUsers.html("<tbody></tbody>");
+        var i = 0;
+        for (var key in users) {
+            tableUsers.append('<tr><td>' + (++i) + '</td><td>' + users[key] + '</td></tr>');
+        }
+    });
+
+    socket.on('update_rooms', function (rooms) {
+        var roomNumber = Object.keys(rooms).length;
+        var tableRooms = $("#table-rooms");
+        $("#rooms-badge").html(roomNumber);
+        tableRooms.html("<tbody></tbody>");
+        var i = 0;
+        for (var key in rooms) {
+            tableRooms.append('<tr><td>' + (++i) + '</td><td>' + key + '</td></tr>');
+        }
+    });
+
+    socket.on('update_chat', function (data) {
+        var conversation = $('#conversation');
+        for (var key in data) {
+            conversation.append('<b>' + data[key].name + ':</b> ' + data[key].msg + '<br>');
+        }
+        conversation.scrollTop(conversation[0].scrollHeight);
+    });
+
+    socket.on('update_wb', function (data, queue) {
+        if (queue) {
+            (function (data, queue) {
+                whiteboard.onSocketMessage(data);
+                whiteboard.history()._queue = queue;
+            }(data, queue));
+        } else {
+            whiteboard.onSocketMessage(data);
+        }
+    });
+
+    socket.on('update_wb_history', function (operation) {
+        operation == "undo" ? whiteboard.history().undo() : whiteboard.history().redo();
+    });
+
+    socket.on('update_image', function (src) {
+        if (src.push) {
+            for (var i = 0; i < src.length; i++) {
+                whiteboard.drawImage(src[i]);
+            }
+        }
+        else {
+            whiteboard.drawImage(src);
+        }
+    });
+
 }())
