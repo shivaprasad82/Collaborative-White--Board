f306c9db13d6e0d689a4d68724cdfa0276796fbc
rajeshwarrao1253 <rajeshwarrao1253@gmail.com>
2023-04-15 13:46:59 -0400

    adding features in whiteboard

--- whiteboard/public/js/whiteboard.js
+++ whiteboard/public/js/whiteboard.js
@@ -57,5 +57,100 @@
             processAction(obj);
         }
     };
+    var onMouseMove = function (ev) {
+        if (!mouseDown) {
+            return false;
+        }
+        var e = ev || window.event;
+        var pos = getMousePos(e);
+        wb.selectedTool.paint(pos, 1);
+        oldX = pos.x;
+        oldY = pos.y;
+    };
+
+    var onMouseUp = function (ev) {
+        if (!mouseDown) {
+            return false;
+        }
+        var e = ev || window.event;
+        var pos = getMousePos(e);
+        wb.selectedTool.paint(pos, 2);
+        mouseDown = false;
+        oldX = null;
+        oldY = null;
+    };
+
+    var stopEventPropagation = function (e) {
+        e.cancelBubble = true;
+        e.returnValue = false;
+        if (typeof (e.preventDefault) === "function") {
+            e.preventDefault();
+        }
+        if (typeof (e.stopPropagation) === "function") {
+            e.stopPropagation();
+        }
+        return false;
+    };
+
+    var enqueue = function (message) {
+        msgQueue.push(message);
+        submitQueue();
+    };
+
+    var drawLine = function (x1, y1, x2, y2) {
+        paintLine(wb.lineWidth, wb.color, x1, y1, x2, y2);
+    };
+
+    var paintLine = function (width, color, x1, y1, x2, y2) {
+        gc.lineCap = wb.lineCap;
+        gc.lineJoin = wb.lineJoin;
+        gc.lineWidth = width || wb.lineWidth;
+        gc.strokeStyle = color || wb.color;
+        gc.beginPath();
+        gc.moveTo(x1, y1);
+        gc.lineTo(x2, y2);
+        gc.stroke();
+    };
+
+    var drawEraser = function (x, y) {
+        paintEraser(x, y);
+    };
+
+    var paintEraser = function (x, y) {
+        gc.globalCompositeOperation = "destination-out";
+        gc.beginPath();
+        gc.arc(x, y, 25, 0, Math.PI * 2);
+        gc.fill();
+        gc.globalCompositeOperation = "source-over";
+    };
+
+    var drawText = function (text, x, y) {
+        paintText(text, wb.color, x, y);
+    };
+
+    var paintText = function (text, color, x, y) {
+        gc.fillStyle = color || wb.color;
+        gc.font = "bold 14px sans-serif";
+        gc.textBaseline = "middle";
+        gc.fillText(text, x, y);
+    };
+
+    var paintRect = function (width, color, left, top, right, bottom) {
+        gc.lineWidth = width || wb.lineWidth;
+        gc.strokeStyle = color || wb.color;
+        gc.beginPath();
+        gc.rect(left, top, right - left, bottom - top);
+        gc.stroke();
+    };
+
+    var clear = function () {
+        paintClear();
+        addCoord(5);
+    };
+
+    var paintClear = function () {
+        gc.clearRect(0, 0, canvas.width, canvas.height);
+    };
+    
 
 }(document.getElementById("canvas")));
